97dca54b29b41673f8c02096d48533fa5c660adc
Gustavo T. Rudiger <gustavo@trudax.tech>
2024-06-09 15:06:42 +0700

    feat: 🎸 use trudax/send-mail actor

--- src/common.js
+++ src/common.js
@@ -1,4 +1,4 @@
-const Apify = require('apify');
+const Apify = require("apify");
 
 const { log } = Apify.utils;
 
@@ -35,10 +35,10 @@ const { log } = Apify.utils;
 
 /** @param {Result} run */
 const isRunResult = (run) => (
-    run
-        && typeof run.hashCode === 'string'
-        && !!run.hashCode
-        && !!run.data
+    run &&
+    typeof run.hashCode === "string" &&
+    !!run.hashCode &&
+    !!run.data
 );
 
 /**
@@ -46,11 +46,15 @@ const isRunResult = (run) => (
  * @returns {(message: string) => string}
  */
 const formatRunMessage = (runResult) => (message) => {
-    const formatted = `${
-        runResult.data.name ? `${runResult.data.name}\n` : ''
-    }${
-        runResult.data.taskName ? `${runResult.data.taskName} - ${runResult.data.actorName}` : runResult.data.actorName
-    }:${runResult.data.buildNumber}\n${createRunLink({ actorId: runResult.data.actId, taskId: runResult.data.taskId, runId: runResult.runId })} : ${message}`;
+    const formatted = `${runResult.data.name ? `${runResult.data.name}\n` : ""}${runResult.data.taskName
+            ? `${runResult.data.taskName} - ${runResult.data.actorName}`
+            : runResult.data.actorName
+        }:${runResult.data.buildNumber}\n${createRunLink({
+            actorId: runResult.data.actId,
+            taskId: runResult.data.taskId,
+            runId: runResult.runId,
+        })
+        } : ${message}`;
     return formatted;
 };
 
@@ -62,7 +66,8 @@ const formatRunMessage = (runResult) => (message) => {
  * }} params
  */
 const createRunLink = ({ actorId, taskId, runId }) => {
-    return `https://my.apify.com/${taskId ? 'tasks' : 'actors'}/${taskId || actorId}/runs/${runId}`;
+    return `https://my.apify.com/${taskId ? "tasks" : "actors"}/${taskId || actorId
+        }/runs/${runId}`;
 };
 
 /**
@@ -75,7 +80,8 @@ const linkToFormat = (body, match) => {
             return out;
         }
 
-        return `${out.slice(0, matches.index)}${out.slice(matches.index).replace(matches[1], match(matches[1]))}`;
+        return `${out.slice(0, matches.index)}${out.slice(matches.index).replace(matches[1], match(matches[1]))
+            }`;
     }, body);
 };
 
@@ -92,9 +98,20 @@ const collectFailed = (result) => {
      */
     const failed = Object.values(result).flatMap((v) => {
         totalSpecs += v?.specs?.length ?? 0;
-        total += v?.specs?.reduce((out, spec) => (out + spec.failedExpectations.length + spec.passedExpectations.length), 0) ?? 0;
+        total +=
+            v?.specs?.reduce(
+                (
+                    out,
+                    spec,
+                ) => (out + spec.failedExpectations.length +
+                    spec.passedExpectations.length),
+                0,
+            ) ?? 0;
 
-        if (!v?.specs?.length || !v.specs.some((spec) => spec.failedExpectations.length)) {
+        if (
+            !v?.specs?.length ||
+            !v.specs.some((spec) => spec.failedExpectations.length)
+        ) {
             passingSpecs++;
             return [];
         }
@@ -106,8 +123,13 @@ const collectFailed = (result) => {
             return spec.failedExpectations.map((s) => {
                 return {
                     name: `${v.description} ${spec.description}`,
-                    markdown: `\`\`\`${linkToFormat(s.message, (link) => `<${link}|${link.split('/').pop()}>`)}\`\`\``,
-                    html: linkToFormat(s.message, (link) => `<a href=${link}>${link.split('/').pop()}</a>`),
+                    markdown: `\`\`\`${linkToFormat(s.message, (link) =>
+                        `<${link}|${link.split("/").pop()}>`)
+                        }\`\`\``,
+                    html: linkToFormat(
+                        s.message,
+                        (link) => `<a href=${link}>${link.split("/").pop()}</a>`,
+                    ),
                 };
             });
         });
@@ -129,7 +151,7 @@ const collectFailed = (result) => {
  * @returns {(name: string, separator: string) => string}
  */
 const nameBreak = () => {
-    let last = '';
+    let last = "";
 
     return (name, separator) => {
         if (name !== last) {
@@ -137,7 +159,7 @@ const nameBreak = () => {
             return `${name}${separator}`;
         }
 
-        return '';
+        return "";
     };
 };
 
@@ -153,7 +175,7 @@ const createNotifier = (input) => {
             log.info(`Posting to channel ${input.slackChannel}`);
 
             try {
-                await Apify.call('katerinahronik/slack-message', {
+                await Apify.call("katerinahronik/slack-message", {
                     token: input.slackToken,
                     channel: input.slackChannel,
                     text: slackMessage,
@@ -162,25 +184,25 @@ const createNotifier = (input) => {
                     waitSecs: 1,
                 });
             } catch (e) {
-                log.exception(e, 'Slack message');
+                log.exception(e, "Slack message");
             }
         }
 
-        if (input.email?.trim().includes('@') && emailMessage && subject) {
+        if (input.email?.trim().includes("@") && emailMessage && subject) {
             log.info(`Sending email to ${input.email}`);
 
             try {
-                await Apify.call('apify/send-mail', {
+                await Apify.call("trudax/send-email", {
                     to: input.email.trim(),
                     subject,
-                    text: '',
+                    text: "",
                     html: emailMessage,
                 }, {
                     fetchOutput: false,
                     waitSecs: 1,
                 });
             } catch (e) {
-                log.exception(e, 'Send email');
+                log.exception(e, "Send email");
             }
         }
     };
